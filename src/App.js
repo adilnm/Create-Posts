1065f31bdd0c73ae099c9122aae5bcc749308f63
Adil Naimi <adilnm23@gmail.com>
2020-10-09 03:08:27 -0500

    Create /new route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React from "react";
+import PostCreate from "./components/PostCreate";
+import { BrowserRouter, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      App
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path="/new" component={PostCreate} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
