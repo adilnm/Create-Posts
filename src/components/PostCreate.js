c61fcdb44ab197df304355d1fd94c754900504e5
Adil Naimi <adilnm23@gmail.com>
2020-10-11 23:43:02 -0500

    add state

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class PostCreate extends React.Component{
+
+    constructor(props){
+        super()
+        this.state={
+            content:''
+        }
+    }
+
+    handleChange=(e)=>{
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
     render(){
         return (
           <div className="PostCreate">
             <form>
-                <textarea type="textarea" placeholder="Content"/><br/>
+                <textarea onChange={this.handleChange} type="textarea" placeholder="Content" name="content"/><br/>
                 <input type="submit"/>
             </form>
           </div>
