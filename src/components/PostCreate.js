889404e1c73d7ae2df267fca87b1369614e0f314
Adil Naimi <adilnm23@gmail.com>
2020-10-12 23:23:02 -0500

    Connect user to the backend

--- src/components/PostCreate.js
+++ src/components/PostCreate.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React from "react";
 
-class PostCreate extends React.Component{
+class PostCreate extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      content: "",
+    };
+  }
 
-    constructor(props){
-        super()
-        this.state={
-            content:''
-        }
-    }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    handleChange=(e)=>{
-        this.setState({
-            [e.target.name]:e.target.value
-        })
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: this.state
+    });
+  };
 
-    render(){
-        return (
-          <div className="PostCreate">
-            <form>
-                <textarea onChange={this.handleChange} type="textarea" placeholder="Content" name="content"/><br/>
-                <input type="submit"/>
-            </form>
-          </div>
-        );
-      }
+  render() {
+    return (
+      <div className="PostCreate">
+        <form onSubmit={this.handleSubmit}>
+          <textarea
+            onChange={this.handleChange}
+            type="textarea"
+            placeholder="Content"
+            name="content"
+          />
+          <br />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default PostCreate
+export default PostCreate;
