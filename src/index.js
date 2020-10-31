5176c487ee30c2a1fefa0fea49dae065af2e90eb
Adil Naimi <adilnm23@gmail.com>
2020-10-30 22:31:36 -0500

    Setup redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'; 
+import reducers from './reducers';
+import { combineReducers } from "redux";
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(()=>{},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
